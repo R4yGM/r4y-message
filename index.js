954a7cf90d0bbd7d4b06704d574072acbc62f824
R4yan <45489155+R4yGM@users.noreply.github.com>
2019-08-22 19:17:18 +0200

    Update index.js

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const PORT = process.env.PORT || 3000;
 //const INDEX = path.join(__dirname, 'index.ejs');
 
 const server = express()
-  .use((req, res) => res.sendFile("index.ejs") )
+  .use((req, res) => res.sendFile("views/index.ejs") )
   .listen(PORT, () => console.log(`Listening on ${ PORT }`));
 
 const io = socketIO(server);
